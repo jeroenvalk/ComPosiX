b513891ddbdb90dc44a1f231fdf7752ff4ab84fd
Jeroen Valk <jeroen.valk@appythings.nl>
2017-02-01 11:20:22 +0100

    create test server

---
+++ src/test/webapp/server.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var http = require('http');
+var options = {
+    // key: fs.readFileSync('server-key.pem'),
+    // cert: fs.readFileSync('server-crt.pem'),
+    // ca: fs.readFileSync('ca-crt.pem'),
+};
+http.createServer(function (req, res) {
+    console.log(new Date()+' '+
+        req.connection.remoteAddress+' '+
+        req.method+' '+req.url);
+    res.writeHead(200);
+    res.end("hello world\n");
+}).listen(8080);
