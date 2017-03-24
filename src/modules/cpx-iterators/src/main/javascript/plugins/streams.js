9a414aff682b6a07054b9b6a7cd3fa39da50525c
Jeroen Valk <jeroen.valk@appythings.nl>
2017-03-24 17:33:45 +0100

    implemented composite streaming

--- src/modules/cpx-iterators/src/main/javascript/plugins/streams.js
+++ src/modules/cpx-iterators/src/main/javascript/plugins/streams.js
@@ -19,12 +19,148 @@ module.exports = function (_) {
     'use strict';
 
     var Node = require("../Node");
+    var stream = require('stream');
+    var http = require('http');
+    var Readable = stream.Readable, PassThrough = stream.PassThrough;
+
+    const writable = function (array) {
+        const readable = (array.length > 0 && (typeof array[0] === "string" || array[0] instanceof Buffer)) ? new PassThrough() : new PassThrough({objectMode: true});
+        readable.on('data', function (data) {
+            data = _.writable(data);
+            array.push(data);
+        });
+        return readable;
+    };
+
+    const readable = function (array) {
+        const writable = (array.length > 0 && (typeof array[0] === "string" || array[0] instanceof Buffer)) ? new PassThrough() : new PassThrough({objectMode: true});
+        const stream = function (readable, offset, count) {
+            if (readable instanceof Array) {
+                for (let i = offset; i < readable.length; ++i) {
+                    count = stream(readable[i], 0, count);
+                    if (count instanceof Promise) {
+                        return count.then(function (count) {
+                            return stream(readable, ++i, count);
+                        });
+                    }
+                }
+                writable.end();
+                return count;
+            }
+            if (readable instanceof Promise) {
+                return readable.then(function (readable) {
+                    return stream(readable, 0, count);
+                });
+            }
+            readable = _.readable(readable);
+            writable.write(readable);
+            return ++count;
+        }
+        stream(array, 0, 0);
+        return writable;
+    };
 
     _.mixin({
+        writable: function (object, writable) {
+            if (Object.getPrototypeOf(object) === Object.prototype) {
+                if (Object.getPrototypeOf(writable) !== Object.prototype) {
+                    throw new Error("object expected");
+                }
+                let result = [];
+                for (const key in writable) {
+                    if (writable.hasOwnProperty(key)) {
+                        if (writable[key] instanceof Readable) {
+                            object[key] = [];
+                        } else {
+                            object[key] = {};
+                        }
+                        result.push(_.writable(object[key], writable[key]));
+                    }
+                }
+                return Promise.all(result);
+            }
+            if (object instanceof Array) {
+                return new Promise(function(resolve, reject) {
+                    if (!(writable instanceof Readable)) {
+                        throw new Error("stream expected");
+                    }
+                    let result = [];
+                    writable.on("data", function (writable) {
+                        if (Object.getPrototypeOf(writable) === Object.prototype) {
+                            const value = {};
+                            object.push(value);
+                            result.push(_.writable(value, writable));
+                        } else {
+                            object.push(writable);
+                        }
+                    });
+                    writable.on("end", function () {
+                        Promise.all(result).then(function(value) {
+                            resolve(value);
+                        });
+                    });
+                });
+            }
+        },
+        readable: function (object) {
+            if (Object.getPrototypeOf(object) === Object.prototype) {
+                let keys = [], values = [], result = {};
+                for (const key in object) {
+                    if (object.hasOwnProperty(key)) {
+                        const value = object[key];
+                        if (value instanceof Promise) {
+                            keys.push(key);
+                            values.push(value);
+                        } else {
+                            result[key] = _.readable(value);
+                        }
+                    }
+                }
+                if (!keys.length) {
+                    return result;
+                }
+                return Promise.all(values).then(function (values) {
+                    for (let i = 0; i < keys.length; ++i) {
+                        result[keys[i]] = _.readable(values[i]);
+                    }
+                    return result;
+                });
+            }
+            if (object instanceof Array) {
+                return readable(object);
+            }
+            return object;
+        },
+        request: function cpxIterators$request(readable, writable) {
+            let todo = 0, done = false;
+            readable.on('data', function (options) {
+                ++todo;
+                const body = options.body;
+                delete options.body;
+                body.pipe(http.request(options, function (res) {
+                    const body = new stream.PassThrough();
+                    writable.write({
+                    //    headers: res.headers,
+                        body: body
+                    });
+                    res.pipe(body);
+                    if (!--todo && done) {
+                        writable.end();
+                    }
+                }));
+            });
+            readable.on('end', function() {
+                if (todo) {
+                    done = true;
+                } else {
+                    writable.end();
+                }
+            });
+        },
         node: function cpxIterators$node(fn, modes) {
             return new Node(fn, modes);
         },
-        pipe: function(source, target) {
+        pipe: function (source, target) {
             return source.pipe(target);
         }
     }, {
--- src/modules/cpx-iterators/src/test/specs/plugins/streams.spec.js
+++ src/modules/cpx-iterators/src/test/specs/plugins/streams.spec.js
@@ -30,98 +30,167 @@ describe('streams', _.globals(function (use) {
 
     let node, $$, request;
 
-    it("local", function () {
-        // do not pipe streams but pipe NODES!
-        let source = _.node(function (writable) { // create a write node
-            writable.write("Hello");
-            writable.write("World!"); // that writes two utf8 data chunks
+    describe("writable", function() {
+        it("simple", function(done) {
+            const x = [""], writable = new stream.PassThrough();
+            let flag = false;
+            _(x).writable(writable).then(function() {
+                expect(flag).to.equal(true);
+                done();
+            }).catch(done);
+            writable.write("Hello World!");
             writable.end();
-        }, 1);
-        node = function () {
-            return _.node(function (readable, writable) { // and pipe it
-                let array = [];   // into another node that
-                readable.on("data", function (data) {
-                    array.push(data.toString());  // collects all chunks
-                });   // accepting either strings or buffers
-                readable.on("end", function () {
-                    writable.write(array.join(" ")); // and joins them together
-                    writable.end();
-                });
-            })
-        };
-        source.pipe(node()).pipe(_.node(function (readable) {
-            readable.on("data", function (data) { // and pipe it again
-                expect(data).to.equal("Hello World!"); // to check out the result
-            });
-        }));
+            expect(x).to.deep.equal(["", new Buffer("Hello World!")]);
+            flag = true;
+            //expect(flag).to.equal(false);
+        });
+
+        it("nested", function(done) {
+            const x = [], writable = new stream.PassThrough({objectMode: true});
+            let flag = false;
+            let readable = new stream.PassThrough();
+            _(x).writable(writable).then(function() {
+                expect(flag).to.deep.equal(true);
+                done();
+            }).catch(done);
+            writable.write({readable: readable});
+            writable.end();
+            readable.write("Hello World!");
+            readable.end();
+            expect(x).to.deep.equal([{readable: [new Buffer("Hello World!")]}])
+            flag = true;
+        });
     });
 
-    it("swagger", function (done) {
-        let EOF = _.node(function (writable) {
-            writable.end();
-        }, 1);
-        request = function (uri) {
-            return _.node(function (readable, writable) {
-                const req = http.request(_.extend({method: "GET", headers: {}}, url.parse(uri)), function (res) {
-                    res.pipe(writable);
-                });
-                req.end();
-            });
-        };
-        let result = new stream.PassThrough();
-        _(EOF).pipe(request("http://localhost:8080/cpx/api/petstore/swagger.json")).pipe(result);
-        result.on("data", function (data) {
-            expect(JSON.parse(data.toString()).swagger).to.equal("2.0");
+    describe("readable", function() {
+        it("simple", function(done) {
+            const x = [];
+            _(x).writable(_(["Hello World!"]).readable()).then(function() {
+                expect(x).to.deep.equal([new Buffer("Hello World!")]);
+                done();
+
+            }).catch(done);
         });
-        result.on("end", function () {
-            done();
+
+        it("nested", function(done) {
+            const x = [];
+            _(x).writable(_([{readable: ["Hello World!"]}]).readable()).then(function() {
+               expect(x).to.deep.equal([{readable: [new Buffer("Hello World!")]}]);
+               done();
+            }).catch(done);
         })
     });
 
-    xit("Google", function () {
-        // imagine the cool things you can do with that
-        $$ = function (selector) {
-            return _.node(function (readable, writable) {
-                _(readable).pipe(node).pipe(function (readable, writable) {
-                    readable.on("")
-                })
-            });
-        };
-
-        // and now lets check out Google
-        let request = function (url) {
-            return _.node(function (readable, writable) {
-                readable.pipe(http.get(url, function (res) {
-                    res.pipe(writable);
-                }));
-            });
-        };
-        request("http://www.google.com/").pipe($$("div.jsb input")).pipe(_.node(function (readable) {
-            readable.once("data", function (data) {
-                expect(data.value).to.equal("Google Search");
-                readable.once("data", function (data) {
-                    expect(data.value).to.equal("I'm Feeling Lucky");
-                });
-            });
-        }));
+    it("request", function(done) {
+        const x = [], writable = new stream.PassThrough({objectMode: true});
+        _(x).writable(writable).then(function() {
+            expect(JSON.parse(x[0].body.toString()).swagger).to.equal("2.0");
+            done();
+        }).catch(done);
+        _(_([{
+            protocol: "http:",
+            hostname: "composix.nl",
+            port: 80,
+            path: "/composix/api/petstore/swagger.json",
+            method: "GET",
+            body: [""]
+        }]).readable()).request(writable);
     });
 
-    xit("PartyAgenda", function () {
-        _.node({
-            "salsa_nl": request("http://www.salsa.nl/uitgaansagendas/latin-agenda.php").pipe($$({
-                "div.ibox": {
-                    "div.agenda": ["h2.highlight", "p"]
-                }
-            })).pipe(function (readable, writable) {
-                readable.on("data", function (readable) {
-                    readable.on("data", function (readable) {
-                        // TODO: think on how to structure this
+    describe("node", function() {
+        it("local", function () {
+            // do not pipe streams but pipe NODES!
+            let source = _.node(function (writable) { // create a write node
+                writable.write("Hello");
+                writable.write("World!"); // that writes two utf8 data chunks
+                writable.end();
+            }, 1);
+            node = function () {
+                return _.node(function (readable, writable) { // and pipe it
+                    let array = [];   // into another node that
+                    readable.on("data", function (data) {
+                        array.push(data.toString());  // collects all chunks
+                    });   // accepting either strings or buffers
+                    readable.on("end", function () {
+                        writable.write(array.join(" ")); // and joins them together
+                        writable.end();
+                    });
+                })
+            };
+            source.pipe(node()).pipe(_.node(function (readable) {
+                readable.on("data", function (data) { // and pipe it again
+                    expect(data).to.equal("Hello World!"); // to check out the result
+                });
+            }));
+        });
+
+        xit("swagger", function (done) {
+            let EOF = _.node(function (writable) {
+                writable.end();
+            }, 1);
+            request = function (uri) {
+                return _.node(function (readable, writable) {
+                    const req = http.request(_.extend({method: "GET", headers: {}}, url.parse(uri)), function (res) {
+                        res.pipe(writable);
+                    });
+                    req.end();
+                });
+            };
+            let result = new stream.PassThrough();
+            _(EOF).pipe(request("http://composix.nl/composix/api/petstore/swagger.json")).pipe(result);
+            result.on("data", function (data) {
+                expect(JSON.parse(data.toString()).swagger).to.equal("2.0");
+            });
+            result.on("end", function () {
+                done();
+            })
+        });
+
+        xit("Google", function () {
+            // imagine the cool things you can do with that
+            $$ = function (selector) {
+                return _.node(function (readable, writable) {
+                    _(readable).pipe(node).pipe(function (readable, writable) {
+                        readable.on("")
                     })
                 });
-            }),
-            "latinnet": request("http://www.latinnet.nl/")
-        }).pipe(function (readable, salsa_nl, latinnet) {
+            };
 
+            // and now lets check out Google
+            let request = function (url) {
+                return _.node(function (readable, writable) {
+                    readable.pipe(http.get(url, function (res) {
+                        res.pipe(writable);
+                    }));
+                });
+            };
+            request("http://www.google.com/").pipe($$("div.jsb input")).pipe(_.node(function (readable) {
+                readable.once("data", function (data) {
+                    expect(data.value).to.equal("Google Search");
+                    readable.once("data", function (data) {
+                        expect(data.value).to.equal("I'm Feeling Lucky");
+                    });
+                });
+            }));
+        });
+
+        xit("PartyAgenda", function () {
+            _.node({
+                "salsa_nl": request("http://www.salsa.nl/uitgaansagendas/latin-agenda.php").pipe($$({
+                    "div.ibox": {
+                        "div.agenda": ["h2.highlight", "p"]
+                    }
+                })).pipe(function (readable, writable) {
+                    readable.on("data", function (readable) {
+                        readable.on("data", function (readable) {
+                            // TODO: think on how to structure this
+                        })
+                    });
+                }),
+                "latinnet": request("http://www.latinnet.nl/")
+            }).pipe(function (readable, salsa_nl, latinnet) {
+
+            });
         });
     });
 
