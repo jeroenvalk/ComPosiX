7f19b0b692395aa5468ef1c095df794a14dce2c1
Jeroen Valk <jeroen.valk@appythings.nl>
2018-01-23 18:15:45 +0100

    internalized json2yaml: https://www.npmjs.com/package/json2yaml

--- src/main/javascript/plugins/yaml.js
+++ src/main/javascript/plugins/yaml.js
@@ -17,8 +17,117 @@
 
 module.exports = function(_) {
 	const yaml = require('js-yaml');
+	const typeOf = require('remedial').typeOf;
 
-	_.module("yaml", ["channel"], function(channel) {
+	_.module("YAML", function () {
+		// modified version of json2yml: https://www.npmjs.com/package/json2yaml
+		"use strict";
+
+		function stringify(data) {
+			var handlers
+				, indentLevel = ''
+			;
+
+			handlers = {
+				"undefined": function () {
+					// objects will not have `undefined` converted to `null`
+					// as this may have unintended consequences
+					// For arrays, however, this behavior seems appropriate
+					return 'null';
+				}
+				, "null": function () {
+					return 'null';
+				}
+				, "number": function (x) {
+					return x;
+				}
+				, "boolean": function (x) {
+					return x ? 'true' : 'false';
+				}
+				, "string": function (x) {
+					// to avoid the string "true" being confused with the
+					// the literal `true`, we always wrap strings in quotes
+					return JSON.stringify(x);
+				}
+				, "array": function (x) {
+					var output = ''
+					;
+
+					if (0 === x.length) {
+						output += '[]';
+						return output;
+					}
+
+					x.forEach(function (y) {
+						// TODO how should `undefined` be handled?
+						var handler = handlers[typeOf(y)]
+						;
+
+						if (!handler) {
+							throw new Error('what the crap: ' + typeOf(y));
+						}
+
+						indentLevel = indentLevel.replace(/$/, '    ');
+						y = handler(y);
+						indentLevel = indentLevel.replace(/    /, '');
+
+						output += '\n' + indentLevel + '- ' + y;
+
+					});
+
+					return output;
+				}
+				, "object": function (x) {
+					var output = ''
+					;
+
+					if (0 === Object.keys(x).length) {
+						output += '{}';
+						return output;
+					}
+
+					Object.keys(x).forEach(function (k) {
+						var val = x[k]
+							, handler = handlers[typeOf(val)]
+						;
+
+						if ('undefined' === typeof val) {
+							// the user should do
+							// delete obj.key
+							// and not
+							// obj.key = undefined
+							// but we'll error on the side of caution
+							return;
+						}
+
+						if (!handler) {
+							throw new Error('what the crap: ' + typeOf(val));
+						}
+
+						indentLevel = indentLevel.replace(/$/, '    ');
+						val = handler(val);
+						indentLevel = indentLevel.replace(/    /, '');
+
+						output += '\n' + indentLevel + k + ': ' + val;
+					});
+
+					output += '\n';
+
+					return output;
+				}
+				, "function": function () {
+					// TODO this should throw or otherwise be ignored
+					return '[object Function]';
+				}
+			};
+
+			return '---' + handlers[typeOf(data)](data) + '\n';
+		}
+
+		return {stringify: stringify};
+	});
+
+	_.module("yaml", ["channel", "YAML"], function(channel, YAML) {
 		const i = channel.create(), o = channel.create(true);
 
 		const recurse = function() {
@@ -37,6 +146,7 @@ module.exports = function(_) {
 		recurse();
 
 		return {
+			stringify: YAML.stringify,
 			rd: o.rd,
 			wr: i.wr
 		};
